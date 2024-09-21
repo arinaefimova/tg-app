dee122ef7ac72f3718c6f6ad4a67b3e7aa8c7089
arina <arina.efimova162002@mail.ru>
2024-09-21 23:06:20 +0300

    add docker files

--- bot.js
+++ bot.js
@@ -1,21 +1,20 @@
+
 import TelegramBot from 'node-telegram-bot-api';
-// replace the value below with the Telegram token you receive from @BotFather
-const token = '7661095573:AAGWXACg21rDjE1n-yX0ERVsZ_upvEJJFRk';
-const url = '91.122.33.43:3000';
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+
+const token = '7661095573:AAGWXACg21rDjE1n-yX0ERVsZ_upvEJJFRk'; // Вставьте ваш токен
+const url = 'https://arinaefimova.github.io/tg-app/'; // Ваш URL
+const bot = new TelegramBot(token, { webHook: true, polling: true });
+
+// Установите webhook на ваш URL
+bot.setWebHook(`${url}/bot${token}`);
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
-
-
   const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
+  const resp = match[1]; 
   bot.sendMessage(chatId, resp);
 });
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-
   bot.sendMessage(chatId, 'Received your message');
 });
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,59 +1,63 @@
 import React, { useState, useEffect } from "react";
 import Community from "../../components/Community/Community";
-import './Home.scss';
-const tg = window.Telegram.WebApp
+import "./Home.scss";
+const tg = window.Telegram.WebApp;
 const Home = ({ communityOpen }) => {
-    const [messages, setMessages] = useState([]);
-    const handleImageUpload = (event) => {
-        const file = event.target.files[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onloadend = () => {
-                setMessages(prevMessages => [...prevMessages, { type: 'image', content: reader.result }]);
-            };
-            reader.readAsDataURL(file);
-        }
-    };
+	return (
+		<main>
+			<div className="home__inner">
+				<div className="container box">
+					<div className="user__from">
+						<div className="user__image">
+							<img
+								src="https://e0.edimdoma.ru/data/posts/0002/1429/21429-ed4_wide.jpg?1631194036"
+								alt="image"
+							/>
+						</div>
+					</div>
+					<div className="bot__from">
+						<div className="bot__image">
+							<img
+								src="https://cdn-icons-png.flaticon.com/512/219/219969.png"
+								alt="img"
+							/>
+						</div>
+						<div className="bot__text">
+							<div className="wrap">
+								<div className="wrap__content">
+									<div className="wrap__title">
+										Пицца (тесто, сыр, томат, соус)
+									</div>
+									<div className="wrap__info">
+										<p>Общие значения по всем продуктам:</p>
+										<p>Калории: 1200</p>
+										<p>Белки: 50.0 г (17%)</p>
+										<p>Жиры: 60.0 г (45%)</p>
+										<p>Углеводы: 100.0 г (38%) </p>
+										<p>Общий вес: 300 г</p>
+										<p>Гликемический индекс: 55.0 </p>
+									</div>
+									<div className="wrap__attention">
+										Внимание! Продукт содержит белково-жировые единицы. В
+										зависимости от общего количества жирной пищи, может
+										потребоваться дополнительная компенсация белков и жиров
+										через 2-3 часа!
+									</div>
+									<div className="wrap__kbj">
+										Количество белково-жировых единиц: 4.0 г (БЖЕ)
+									</div>
+									<div>Приятного аппетита!</div>
+								</div>
+								<div className="wrap__button">Записать в дневник</div>
+							</div>
+						</div>
+					</div>
+				</div>
 
-    const getRandomResponse = () => {
-        const responses = ["Привет!", "Как дела?", "Чем могу помочь?"];
-        return responses[Math.floor(Math.random() * responses.length)];
-    };
-
-    const handleSendMessage = () => {
-        const response = getRandomResponse();
-        setMessages(prevMessages => [...prevMessages, { type: 'text', content: response }]);
-    };
-
-
-    useEffect(() => {
-        
-       tg.ready()
-    }, []);
-
-    const onClose=()=>{
-        tg.Close()
-
-    }
-
-    return (
-        <main>
-            <div className="home__inner">
-
-                <button onClick={onClose}>Закрыть</button>
-                {/* <input type="file" accept="image/*" onChange={handleImageUpload} />
-                <button onClick={handleSendMessage}>Отправить сообщение</button>
-                <div className="chat"> */}
-                    {/* {messages.map((msg, index) => (
-                        <div key={index} className={msg.type === 'image' ? 'chat__image' : 'chat__text'}>
-                            {msg.type === 'image' ? <img src={msg.content} alt="user upload" /> : msg.content}
-                        </div>
-                    ))} */}
-                {/* </div> */}
-                {communityOpen && <Community />}
-            </div>
-        </main>
-    );
+				{communityOpen && <Community />}
+			</div>
+		</main>
+	);
 };
 
 export default Home;
